b0f482ec99734fb6f9c579532f2acfdeafe7ee6f
shisiye15@163.com <shisiye16@163.com>
2021-11-09 16:26:03 +0800

    fix the version of compiler to 0.6.12

--- trias_heco.sol
+++ trias_heco.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.0;
+pragma solidity 0.6.12;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.4.0/contracts/token/ERC20/ERC20.sol";
 
